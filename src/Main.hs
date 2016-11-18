7adf1c38b180223cb677fc09c5d158bc60c49b9c
Cyrill Schenkel <cyrill.schenkel@gmail.com>
2016-11-18 15:36:45 +0100

    Add one-line comments.
    * src/Main.hs (repl): Check if read line is a comment.
      (isCommentLine): New function.

--- src/Main.hs
+++ src/Main.hs
@@ -14,19 +14,24 @@ repl context =
      eof <- isEOF
      if eof
        then putStrLn "Bye!"
-       else readEvalPrint context >>= repl
+       else readEvalPrint context
   where
     readEvalPrint context =
       do line <- getLine
-         if isEmptyLine line
-           then readEvalPrint context
+         if isEmptyLine line || isCommentLine line
+           then putStrLn "" >> repl context
            else case lambdaRead line of
                   Left e ->
-                    (putStrLn $ show e) >> return context
+                    (putStrLn $ show e) >> return context >>= repl
                   Right term -> do
                     let (context', term') = lambdaEval context term in
-                      (putStrLn $ show term') >> return context'
+                      (putStrLn $ show term') >> repl context'
 
 isEmptyLine :: String -> Bool
 isEmptyLine [] = True
 isEmptyLine _ = False
+
+isCommentLine :: String -> Bool
+isCommentLine [] = False
+isCommentLine ('#':_) = True
+isCommentLine _ = False
